1ba4a2df58b365b3e1ee6d660f30bed4ce10fdf0
adnan5509 <adnanafzal5509@gmail.com>
2025-04-15 22:08:39 +0200

    refactor MessagesService and NewMessageComponent for improved message handling and state management

--- src/app/messages/new-message/new-message.component.ts
+++ src/app/messages/new-message/new-message.component.ts
@@ -13,7 +13,7 @@ import { MessagesService } from '../message.service';
 export class NewMessageComponent {
 
   private messageService = inject(MessagesService);
-  enteredText = signal('');
+  enteredText = '';
 
   get debugOutput() {
     console.log('[NewMessage] "debugOutput" binding re-evaluated.');
@@ -21,7 +21,7 @@ export class NewMessageComponent {
   }
 
   onSubmit() {
-    this.messageService.addMessage(this.enteredText());
-    this.enteredText.set('');
+    this.messageService.addMessage(this.enteredText);
+    this.enteredText = '';
   }
 }
