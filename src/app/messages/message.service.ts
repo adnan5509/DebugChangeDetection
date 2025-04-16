34c6ddd1c38756f107311c95aa449ee523669184
adnan5509 <adnanafzal5509@gmail.com>
2025-04-16 15:14:14 +0200

    refactor MessagesService to use BehaviorSubject for message updates and enhance MessagesListComponent with OnInit and OnDestroy lifecycle hooks for better subscription management

--- src/app/messages/message.service.ts
+++ src/app/messages/message.service.ts
@@ -1,15 +1,18 @@
 import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessagesService {
   private messages: string[] = [];
+  messages$ = new BehaviorSubject<string[]>([]);
   get allMessages() {
     return this.messages;
   }
 
   addMessage(message: string) {
+    this.messages$.next([...this.messages, message]);
     this.messages = [message, ...this.messages];
   }
 }
--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { MessagesService } from '../message.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-messages-list',
@@ -8,9 +9,26 @@ import { MessagesService } from '../message.service';
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent {
+export class MessagesListComponent implements OnInit, OnDestroy {
+
   private messageService = inject(MessagesService);
-  messages = this.messageService.allMessages;
+
+  private cdRef = inject(ChangeDetectorRef);
+  messages: string[] = [];
+  subscribtions: Subscription[] = [];
+
+  ngOnInit() {
+    this.subscribtions.push(this.messageService.messages$.subscribe((messages) => {
+      this.messages = messages;
+      this.cdRef.markForCheck();
+    }));
+  }
+
+  ngOnDestroy() {
+    this.subscribtions.forEach(sub => {
+      sub.unsubscribe();
+    });
+  }
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
